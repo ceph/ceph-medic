a4af53dec4250ea37d24806ba07c8c27c5b68366
Alfredo Deza <adeza@redhat.com>
2016-11-28 14:21:57 -0500

    rules: create example rule files
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_doctor/rules/kraken.py
@@ -0,0 +1,15 @@
+# Rules to apply for Jewel releases.
+
+# All checks are applied, but overrides to defaults can
+# be specified here.
+# overrides = {
+#     # overrides the check called 'check_name' with a different expected value
+#     # and changes # the level of this check to 'error'.
+#     "check_name": {"expected": "value", "level": "error"},
+#}
+
+# Exclude the following checks:
+# excludes = ["check_name"]
+
+# Include the following checks:
+# includes = ["check_name"]
