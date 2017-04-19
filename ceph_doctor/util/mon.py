de386b42f48af9dc65ff7c03766c046c5df78c79
Alfredo Deza <adeza@redhat.com>
2017-04-19 14:21:51 -0400

    util: create a mon module, for fetching cluster information
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_doctor/util/mon.py
@@ -0,0 +1,86 @@
+import remoto
+import json
+import ceph_doctor
+
+
+def get_mon_report(conn):
+    out, err, code = remoto.process.check(
+        conn,
+        [
+            'ceph',
+            '--cluster=%s' % ceph_doctor.config['cluster_name'],
+            'report'
+        ],
+    )
+
+    if code > 0:
+        for line in err:
+            print line
+
+    try:
+        return json.loads(b''.join(out).decode('utf-8'))
+    except ValueError:
+        return {}
+
+
+def get_cluster_nodes(conn):
+    """
+    Ask a monitor (with a pre-made connection) about all the nodes in
+    a cluster. This will be able to get us all known MONs and OSDs.
+
+    It returns a dictionary with a mapping that looks like::
+
+        {
+            'mons': [
+                {
+                    'host': 'node1',
+                    'public_ip': '192.168.1.100',
+                },
+            ],
+            'osds': [
+                {
+                    'host': 'node2',
+                    'public_ip': '192.168.1.101',
+                },
+                {
+                    'host': 'node3',
+                    'public_ip': '192.168.1.102',
+                },
+            ]
+        }
+
+    """
+    report = get_mon_report(conn)
+    nodes = {'mons': [], 'osds': []}
+    try:
+        # XXX Is this really needed? in what case we wouldn't have a monmap
+        # with mons?
+        mons = report['monmap']['mons']
+    except KeyError:
+        raise SystemExit(report)
+    for i in mons:
+        nodes['mons'].append({
+            'host': i['name'],
+            'public_ip': _extract_ip_address(i['public_addr'])
+        })
+
+    osds = report['osd_metadata']
+    for i in osds:
+        nodes['osds'].append({
+            'host': i['hostname'],
+            'public_ip': _extract_ip_address(i['front_addr'])
+        })
+
+    return nodes
+
+
+# XXX does not support IPV6
+
+def _extract_ip_address(string):
+    """
+    Addresses from Ceph reports can come up with subnets and ports using ':'
+    and '/' to identify them properly. Parse those types of strings to extract
+    just the IP.
+    """
+    port_removed = string.split(':')[0]
+    return port_removed.split('/')[0]
