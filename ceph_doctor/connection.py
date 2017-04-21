2ff277d64e97e59e9b8964fe8786d3f4f38fa9f0
Alfredo Deza <adeza@redhat.com>
2017-04-21 07:09:31 -0700

    connection: check for custom cluster names
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_doctor/connection.py
+++ ceph_doctor/connection.py
@@ -1,5 +1,6 @@
 import socket
 import remoto
+import ceph_doctor
 
 
 def get_connection(hostname, username=None, logger=None, threads=5, use_sudo=None, detect_sudo=True):
@@ -20,9 +21,16 @@ def get_connection(hostname, username=None, logger=None, threads=5, use_sudo=Non
         # Set a timeout value in seconds to disconnect and move on
         # if no data is sent back.
         conn.global_timeout = 300
-        #logger.debug("connected to host: %s " % hostname)
-        return conn
 
+        # XXX put this somewhere else
+        cluster_conf_files, stderr, exit_code = remoto.process.check(conn, ['ls', '/etc/ceph/'])
+        cluster_name = 'ceph'
+        if 'ceph.conf' not in cluster_conf_files:
+            for i in cluster_conf_files:
+                if i.endswith('conf'):
+                    cluster_name = i.split('.conf')[0]
+        ceph_doctor.config['cluster_name'] = cluster_name
+        return conn
     except Exception as error:
         msg = "connecting to host: %s " % hostname
         errors = "resulted in errors: %s %s" % (error.__class__.__name__, error)
