47d6bccff1e2b1e8105bcec75157d21b6f0394c2
Alfredo Deza <adeza@redhat.com>
2017-04-27 14:31:27 -0400

    connection: set remote logging on the connection
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_doctor/connection.py
+++ ceph_doctor/connection.py
@@ -1,19 +1,28 @@
+import logging
 import socket
 import remoto
 import ceph_doctor
+from ceph_doctor import terminal
+
+logger = logging.getLogger(__name__)
 
 
-def get_connection(hostname, username=None, logger=None, threads=5, use_sudo=None, detect_sudo=True):
+def get_connection(hostname, username=None, threads=5, use_sudo=None, detect_sudo=True, **kw):
     """
     A very simple helper, meant to return a connection
     that will know about the need to use sudo.
     """
+    if kw.get('logger') is False:  # explicitly disable remote logging
+        remote_logger = None
+    else:
+        remote_logger = logging.getLogger(hostname)
+
     if username:
         hostname = "%s@%s" % (username, hostname)
     try:
         conn = remoto.Connection(
             hostname,
-            logger=logger,
+            logger=remote_logger,
             threads=threads,
             detect_sudo=detect_sudo,
         )
@@ -26,15 +35,21 @@ def get_connection(hostname, username=None, logger=None, threads=5, use_sudo=Non
         cluster_conf_files, stderr, exit_code = remoto.process.check(conn, ['ls', '/etc/ceph/'])
         cluster_name = 'ceph'
         if 'ceph.conf' not in cluster_conf_files:
+            logger.warning('/etc/ceph/ceph.conf was not found, will try to infer the cluster name')
+            terminal.warning('/etc/ceph/ceph.conf was not found')
             for i in cluster_conf_files:
                 if i.endswith('conf'):
                     cluster_name = i.split('.conf')[0]
-        ceph_doctor.config['cluster_name'] = cluster_name
+                    logger.warning('inferred %s as the cluster name', cluster_name)
+                    terminal.warning('inferred %s as the cluster name' % cluster_name)
+        ceph_doctor.metadata['cluster_name'] = cluster_name
         return conn
     except Exception as error:
         msg = "connecting to host: %s " % hostname
         errors = "resulted in errors: %s %s" % (error.__class__.__name__, error)
-        raise RuntimeError(msg + errors)
+        logger.error(msg)
+        logger.error(errors)
+        raise error
 
 
 def get_local_connection(logger, use_sudo=False):
