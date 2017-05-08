a73678a40982216cc7315b0871b79d70272f5b76
Alfredo Deza <adeza@redhat.com>
2017-05-08 10:20:31 -0400

    expand metadata to all supported node types
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/__init__.py
+++ ceph_medic/__init__.py
@@ -4,6 +4,8 @@ config = {
     'hosts_file': None,
 }
 
-metadata = {'osds':{}, 'mons':{}, 'nodes': {}}
+metadata = {'rgws': {}, 'mgrs': {}, 'mdss':{}, 'clients': {}, 'osds':{}, 'mons':{}, 'nodes': {}}
+
+daemon_types = [i for i in metadata.keys() if i != 'nodes']
 
 __version__ = '0.0.1'
