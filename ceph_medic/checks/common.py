c0238f1d284b2fc7072d05db4539c4e52381ddf9
Alfredo Deza <adeza@redhat.com>
2017-05-10 13:20:05 -0400

    checks: check cluster fsid parity
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/checks/common.py
+++ ceph_medic/checks/common.py
@@ -1,8 +1,21 @@
 from ceph_medic import metadata
 
 
-all_nodes = [metadata['mons'], metadata['osds']]
+#
+# Utilities
+#
 
+def get_fsid(data):
+    contents = data['paths']['/etc/ceph']['files'][cluster_path]['contents']
+    try:
+        fsid_line = [i for i in contents.split('\n') if 'fsid' in i][0].split('=')[-1].strip()
+    except IndexError:
+        return ''
+    return fsid_line
+
+#
+# Error checks
+#
 
 def check_ceph_conf_exists(host, data):
     cluster_conf = '/etc/ceph/%s.conf' % metadata['cluster_name']
@@ -33,3 +46,22 @@ def check_var_lib_ceph_permissions(host, data):
     if group == owner != 'ceph':
         msg = '/var/lib/ceph has invalid ownership:  %s:%s' % (owner, group)
         return code, msg
+
+
+def check_cluster_fsid(host, data):
+    code = 'ECOM5'
+    msg = 'fsid "%s" is different than host(s): %s'
+    cluster_path = '/etc/ceph/%s.conf' % metadata['cluster_name']
+    mismatched_hosts = []
+
+    current_fsid = get_fsid(data)
+
+    for daemon, hosts in metadata['nodes'].items():
+        for host in hosts:
+            hostname = host['host']
+            host_fsid = get_fsid(metadata[daemon][hostname])
+            if current_fsid != host_fsid:
+                mismatched_hosts.append(hostname)
+
+    if mismatched_hosts:
+        return code, msg % (current_fsid, ','.join(mismatched_hosts))
