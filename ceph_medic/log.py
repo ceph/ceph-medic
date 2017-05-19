8c9054f13f470c3a0a0564bb2b273de28ea8bf5a
Alfredo Deza <adeza@redhat.com>
2017-05-19 09:55:13 -0400

    log: allow configuring logging to a file from configuration
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/log.py
+++ ceph_medic/log.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 import logging
+import os
 
 BASE_FORMAT = "[%(name)s][%(levelname)-6s] %(message)s"
 FILE_FORMAT = "[%(asctime)s]" + BASE_FORMAT
 
 
-def setup():
+def setup(config=None):
     root_logger = logging.getLogger()
+    log_path = config.get_safe('global', '--log-path', '.')
+    if not os.path.exists(log_path):
+        raise RuntimeError('configured ``--log-path`` value does not exist: %s' % log_path)
+    date = datetime.strftime(datetime.utcnow(), '%Y-%m-%d')
+    log_file = os.path.join(log_path, 'ceph-medic-%s.log' % date)
 
     root_logger.setLevel(logging.DEBUG)
 
-    date = datetime.strftime(datetime.utcnow(), '%Y-%m-%d')
-
     # File Logger
-    # XXX this needs to have a configurable location
-    fh = logging.FileHandler('ceph-medic-%s.log' % date)
+    fh = logging.FileHandler(log_file)
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(logging.Formatter(FILE_FORMAT))
 
