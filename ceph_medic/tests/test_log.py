bd92e2424e631a4c1d75a522f314cbca828f959b
Alfredo Deza <adeza@redhat.com>
2017-05-22 08:14:40 -0400

    tests: add log setup tests
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_medic/tests/test_log.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from ceph_medic.util import configuration
+from ceph_medic import log
+import logging
+
+
+class TestLogSetup(object):
+
+    def teardown(self):
+        logger = logging.getLogger()
+        logger.handlers = []
+
+    def test_barf_when_config_path_does_not_exist(self, tmpdir):
+        location = os.path.join(str(tmpdir), 'ceph-medic.conf')
+        with open(location, 'w') as _f:
+            _f.write("""\n[global]\n--log-path=/bogus/path""")
+        config = configuration.load(location)
+        with pytest.raises(RuntimeError) as error:
+            log.setup(config)
+        assert 'value does not exist' in str(error)
+
+    def test_create_log_config_correctly(self, tmpdir):
+        tmp_log_path = str(tmpdir)
+        location = os.path.join(tmp_log_path, 'ceph-medic.conf')
+        with open(location, 'w') as _f:
+            _f.write("""\n[global]\n--log-path=%s""" % tmp_log_path)
+        config = configuration.load(location)
+        log.setup(config)
+        logger = logging.getLogger()
+        assert len(logger.handlers) == 1
