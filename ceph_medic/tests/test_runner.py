00334c79a455814debf638da5a852057ceeb4e6a
Alfredo Deza <adeza@redhat.com>
2017-09-12 14:02:41 -0400

    tests add runner output tests for newline verification
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/tests/test_runner.py
+++ ceph_medic/tests/test_runner.py
@@ -1,3 +1,4 @@
+import pytest
 import ceph_medic
 from ceph_medic import runner
 from ceph_medic.tests import base_metadata
@@ -33,15 +34,6 @@ class TestRunner(object):
         assert run.total_hosts == 4
 
 
-class FakeWriter(object):
-
-    def __init__(self):
-        self.calls = []
-
-    def raw(self, string):
-        self.calls.append(string)
-
-
 class TestReport(object):
 
     def setup(self):
@@ -69,3 +61,94 @@ class TestReport(object):
         monkeypatch.setattr(runner.terminal, 'write', fake_writer)
         runner.report(self.results)
         assert fake_writer.calls[0] == '\n0 passed, on 0 hosts'
+
+
+
+class TestReportBasicOutput(object):
+
+    def setup(self):
+        # clear metadata
+        ceph_medic.metadata = base_metadata
+        runner.metadata = base_metadata
+        runner.metadata['nodes'] = {}
+        runner.Runner().run()
+
+    def test_has_version(self, terminal):
+        assert 'Version: ' in terminal.get_output()
+
+    def test_has_cluster_name(self, terminal):
+        assert 'Cluster Name: "ceph"' in terminal.get_output()
+
+    def test_has_no_hosts(self, terminal):
+        assert 'Total hosts: [0]' in terminal.get_output()
+
+    def test_has_a_header(self, terminal):
+        assert '==  Starting remote check session  ==' in terminal.get_output()
+
+    def test_has_no_OSDs(self, terminal):
+        assert 'OSDs:    0' in terminal.get_output()
+
+    def test_has_no_MONs(self, terminal):
+        assert 'MONs:    0' in terminal.get_output()
+
+    def test_has_no_Clients(self, terminal):
+        assert 'Clients:    0' in terminal.get_output()
+
+    def test_has_no_MDSs(self, terminal):
+        assert 'MDSs:    0' in terminal.get_output()
+
+    def test_has_no_MGRs(self, terminal):
+        assert 'MGRs:       0' in terminal.get_output()
+
+    def test_has_no_RGWs(self, terminal):
+        assert 'RGWs:    0' in terminal.get_output()
+
+
+class TestReportErrors(object):
+
+    def setup(self):
+        # clear metadata
+        ceph_medic.metadata = base_metadata
+        runner.metadata = base_metadata
+        runner.metadata['nodes'] = {}
+
+    def test_get_new_lines_in_errors(self, terminal, mon_keyring, data, monkeypatch):
+        data_node1 = data()
+        data_node2 = data()
+        data_node1['paths']['/var/lib/ceph']['files'] = {
+            '/var/lib/ceph/mon/ceph-0/keyring': {'contents': mon_keyring()}
+        }
+        data_node1['paths']['/var/lib/ceph']['dirs'] = {
+            '/var/lib/ceph/osd/ceph-10': {},
+            '/var/lib/ceph/osd/ceph-11': {},
+            '/var/lib/ceph/osd/ceph-12': {},
+            '/var/lib/ceph/osd/ceph-13': {},
+            '/var/lib/ceph/osd/ceph-0': {},
+            '/var/lib/ceph/osd/ceph-1': {},
+            '/var/lib/ceph/osd/ceph-2': {},
+            '/var/lib/ceph/osd/ceph-3': {},
+        }
+
+        data_node2['paths']['/var/lib/ceph']['files'] = {
+            '/var/lib/ceph/mon/ceph-1/keyring': {'contents': mon_keyring()},
+        }
+        data_node2['paths']['/var/lib/ceph']['dirs'] = {
+            '/var/lib/ceph/osd/ceph-10': {},
+            '/var/lib/ceph/osd/ceph-11': {},
+            '/var/lib/ceph/osd/ceph-12': {},
+            '/var/lib/ceph/osd/ceph-13': {},
+            '/var/lib/ceph/osd/ceph-0': {},
+            '/var/lib/ceph/osd/ceph-1': {},
+            '/var/lib/ceph/osd/ceph-2': {},
+            '/var/lib/ceph/osd/ceph-3': {},
+        }
+
+        # set the data everywhere we need it
+        ceph_medic.metadata['mons'] = {'node1': data_node1, 'node2': data_node2}
+        monkeypatch.setattr(ceph_medic.checks.mons, 'metadata', ceph_medic.metadata)
+
+        runner.Runner().run()
+        # Any line that is an error or a warning *must* end with a newline
+        for line in terminal.calls:
+            if line.lstrip().startswith(('E', 'W')):
+                assert line.endswith('\n')
