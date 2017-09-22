4923b8fac7259b984ab9211f423d09f236f511fb
Alfredo Deza <adeza@redhat.com>
2017-09-22 14:27:32 -0400

    tests/checks add tests for the min_pool_size check
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/tests/checks/test_osds.py
+++ ceph_medic/tests/checks/test_osds.py
@@ -1,7 +1,15 @@
+from textwrap import dedent
 from ceph_medic.checks import osds
+from ceph_medic import metadata
 
 
 class TestOSDS(object):
+
+    def setup(self):
+        metadata['cluster_name'] = 'ceph'
+
+    def teardown(self):
+        metadata.pop('cluster_name', None)
 
     def test_fails_check_ceph_fsid(self):
         data = {'paths': {'/var/lib/ceph': {'files': {
@@ -10,3 +18,25 @@ class TestOSDS(object):
         }}}}
         result = osds.check_osd_ceph_fsid(None, data)
         assert "WOSD1" in result
+
+    def test_min_pool_size_fails(self, data):
+        contents = dedent("""
+        [global]
+        cluster = foo
+        osd_pool_default_min_size = 1
+        """)
+        osd_data = data()
+        osd_data['paths']['/etc/ceph']['files']['/etc/ceph/ceph.conf'] = {'contents': contents}
+        code, error = osds.check_min_pool_size(None, osd_data)
+        assert error == 'osd default pool size is set to 1, can potentially lose data'
+
+    def test_min_pool_size_is_correct(self, data):
+        contents = dedent("""
+        [global]
+        cluster = foo
+        osd_pool_default_min_size = 2
+        """)
+        osd_data = data()
+        osd_data['paths']['/etc/ceph']['files']['/etc/ceph/ceph.conf'] = {'contents': contents}
+        result = osds.check_min_pool_size(None, osd_data)
+        assert result is None
