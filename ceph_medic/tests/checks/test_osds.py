bd424a73b458750125dfff7928cb653d72fbfbb4
Jean-Charles Lopez <jelopez@redhat.com>
2019-02-18 09:15:07 -0800

    Update test_osds.py
    Updated the min_size check to reflect change in checks/osds.py

--- ceph_medic/tests/checks/test_osds.py
+++ ceph_medic/tests/checks/test_osds.py
@@ -28,7 +28,7 @@ class TestOSDS(object):
         osd_data = data()
         osd_data['paths']['/etc/ceph']['files']['/etc/ceph/ceph.conf'] = {'contents': contents}
         code, error = osds.check_min_pool_size(None, osd_data)
-        assert error == 'osd default pool size is set to 1, can potentially lose data'
+        assert error == 'osd default pool min_size is set to 1, can potentially lose data'
 
     def test_min_pool_size_is_correct(self, data):
         contents = dedent("""
