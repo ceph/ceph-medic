00de9dea794e9900b4311958c1c204b8605a5ccb
Alfredo Deza <adeza@redhat.com>
2019-02-19 09:18:38 -0500

    config: leave commented out harness
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/__init__.py
+++ ceph_medic/__init__.py
@@ -1,3 +1,19 @@
+
+
+# TODO: make this nicer for a file-based loaded config
+#class UnloadedConfig(object):
+#    """
+#    This class is used as the default value for config.ceph so that if
+#    a configuration file is not successfully loaded then it will give
+#    a nice error message when values from the config are used.
+#    """
+#    def __getattr__(self, *a):
+#        raise RuntimeError("No valid ceph configuration file was loaded.")
+#
+#
+#config = namedtuple('config', ['verbosity', 'nodes', 'hosts_file', 'file'])
+#conf.file = UnloadedConfig()
+
 config = {
     'verbosity': 'info',
     'nodes': {},
