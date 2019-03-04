aec05524e4c08c593bac10000450ab4b60630b82
Alfredo Deza <adeza@redhat.com>
2019-03-04 10:17:56 -0500

    config: move from dict to namedtuple
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_medic/__init__.py
+++ ceph_medic/__init__.py
@@ -1,26 +1,20 @@
+from collections import namedtuple
 
 
-# TODO: make this nicer for a file-based loaded config
-#class UnloadedConfig(object):
-#    """
-#    This class is used as the default value for config.ceph so that if
-#    a configuration file is not successfully loaded then it will give
-#    a nice error message when values from the config are used.
-#    """
-#    def __getattr__(self, *a):
-#        raise RuntimeError("No valid ceph configuration file was loaded.")
-#
-#
-#config = namedtuple('config', ['verbosity', 'nodes', 'hosts_file', 'file'])
-#conf.file = UnloadedConfig()
+class UnloadedConfig(object):
+    """
+    This class is used as the default value for config.ceph so that if
+    a configuration file is not successfully loaded then it will give
+    a nice error message when values from the config are used.
+    """
+    def __getattr__(self, *a):
+        raise RuntimeError("No valid ceph configuration file was loaded.")
 
-config = {
-    'verbosity': 'info',
-    'nodes': {},
-    'hosts_file': None,
-}
 
-metadata = {'rgws': {}, 'mgrs': {}, 'mdss':{}, 'clients': {}, 'osds':{}, 'mons':{}, 'nodes': {}}
+config = namedtuple('config', ['verbosity', 'nodes', 'hosts_file', 'file'])
+config.file = UnloadedConfig()
+
+metadata = {'rgws': {}, 'mgrs': {}, 'mdss': {}, 'clients': {}, 'osds': {}, 'mons': {}, 'nodes': {}}
 
 daemon_types = [i for i in metadata.keys() if i != 'nodes']
 
