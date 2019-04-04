e36a9a66d25f0d2c217baefc6879d6ee4f93d18e
Alfredo Deza <adeza@redhat.com>
2019-04-04 15:53:44 -0400

    tests: verify new cluster module
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_medic/tests/checks/test_cluster.py
@@ -0,0 +1,16 @@
+from ceph_medic.checks import cluster
+from ceph_medic import metadata
+
+
+class TestCheckOSDs(object):
+
+    def setup(self):
+        metadata['cluster_name'] = 'ceph'
+        metadata['osds'] = {}
+
+    def test_no_osds(self):
+        assert cluster.check_osds_exist() == ('ECLS1', 'There are no OSDs available')
+
+    def test_osds_are_found(self):
+        metadata['osds'] = {'osd1': {}}
+        assert cluster.check_osds_exist() is None
