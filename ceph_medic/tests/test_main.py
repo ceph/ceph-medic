8bd9c53aa5870f300bbf0c2db673162e52dc4892
Zack Cerza <zack@redhat.com>
2020-01-24 14:53:34 -0700

    tests/main: Fix unhelpful ssh_config test
    SystemExit is always raised, so this wasn't doing anything. Actually
    look at the output.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- ceph_medic/tests/test_main.py
+++ ceph_medic/tests/test_main.py
@@ -6,7 +6,9 @@ class TestMain(object):
     def test_main(self):
         assert ceph_medic.main
 
-    def test_invalid_ssh_config(self):
+    def test_invalid_ssh_config(self, capsys):
         argv = ["ceph-medic", "--ssh-config", "/does/not/exist"]
         with pytest.raises(SystemExit):
             ceph_medic.main.Medic(argv)
+        out = capsys.readouterr()
+        assert 'the given ssh config path does not exist' in out.out
